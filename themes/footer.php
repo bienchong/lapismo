29df14682ac33410fd797cf76816decb3993e40c
bienchong <bien_imbang@yahoo.com>
2018-10-17 20:52:23 +0800

    repository created. plugins downloaded

---
+++ themes/footer.php
@@ -0,0 +1,7 @@
+
+</main>
+        <footer class="pg-empty-placeholder"></footer>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <?php wp_footer(); ?>
+    </body>
+</html>
